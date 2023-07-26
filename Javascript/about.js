f55330a1fad7e5adf8798178c4b880e28ad4187a
Abhinav Choukse <124260348+Abhinav-378@users.noreply.github.com>
2023-07-26 17:28:01 +0530

    Update about.js

--- Javascript/about.js
+++ Javascript/about.js
@@ -6,3 +6,18 @@ document.addEventListener('scroll',()=>{
         header.classList.remove('scrolled');
     }
 })
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('showEle');
+        }
+        else{
+            entry.target.classList.remove('showEle');
+        }
+        
+    })
+})
+
+const hiddenElements= document.querySelectorAll('.hiddenEle');
+hiddenElements.forEach((el) => observer.observe(el));
